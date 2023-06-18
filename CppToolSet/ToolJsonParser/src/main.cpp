458bd5e3f8f6a7d3d9d7b1471af456a96cf23f3a
keqing996 <epigrf@outlook.com>
2023-06-18 13:53:30 +0800

    add rapid json library and test code, mainly for w_char

--- CppToolSet/ToolJsonParser/src/main.cpp
+++ CppToolSet/ToolJsonParser/src/main.cpp
@@ -1,38 +1,11 @@
 ﻿
-#include <fstream>
-#include <iostream>
-#include <functional>
-
-#include "nlohmann_json/json.hpp"
+#include "nlohmann_json_test.hpp"
+#include "rapid_json_test.hpp"
 
 int main()
 {
-    std::ifstream inputFile("./config/config.json");
-    nlohmann::json jsonConfig;
-    inputFile >> jsonConfig;
-
-    // for each
-    std::cout << "for each: \n";
-
-    using BasicJson = nlohmann::basic_json<>;
-    
-    std::function<void(BasicJson&)> print = [&print](BasicJson& json) -> void
-    {
-        for (const auto& [key, value] : json.items())
-        {
-            std::cout << key << std::endl;
-
-            if (value.is_object() || value.is_array())
-                print(value);
-        }
-    };
-    
-    print(jsonConfig);
-    
-    // dump
-    std::string str = jsonConfig.dump();
-
-    std::cout << str;
+    NlohmannJsonTest();
+    RapidJsonTest();
     
     return 0;
 }
