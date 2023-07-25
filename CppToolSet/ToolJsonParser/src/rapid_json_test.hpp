84e424f18084e77f5460bc146577db63409eba04
keqing996 <epigrf@outlook.com>
2023-07-25 18:50:47 +0800

    update json5 paser

--- CppToolSet/ToolJsonParser/src/rapid_json_test.hpp
+++ CppToolSet/ToolJsonParser/src/rapid_json_test.hpp
@@ -30,3 +30,31 @@ void RapidJsonTest()
         std::wcout << element[L"desc"].GetString() << std::endl;
     }
 }
+
+void RapidJson5Test()
+{
+    std::wifstream inputFile("./config/config.json");
+    std::wstringstream fileContent;
+    fileContent << inputFile.rdbuf();
+    inputFile.close();
+
+    using DocumentW = rapidjson::GenericDocument<rapidjson::UTF16<>>;
+    using ValueW = rapidjson::GenericValue<rapidjson::UTF16<>>;
+
+    constexpr int Json5Parse = rapidjson::kParseCommentsFlag | rapidjson::kParseTrailingCommasFlag;
+
+    DocumentW doc;
+    doc.Parse<Json5Parse>(fileContent.str().c_str());
+
+    std::wcout << doc[L"tag1"].GetString() << std::endl;
+    std::wcout << doc[L"tag2"].GetBool() << std::endl;
+
+    const ValueW& arrayNode = doc[L"array1"];
+    size_t arrLength = arrayNode.Size();
+    for (int i = 0; i < arrLength; i++)
+    {
+        const ValueW& element = arrayNode[i];
+        std::wcout << element[L"name"].GetString() << std::endl;
+        std::wcout << element[L"desc"].GetString() << std::endl;
+    }
+}
