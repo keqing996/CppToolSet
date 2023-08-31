2c2115b2f09b1bea1bbd5a3e9cec7492c9de156b
keqing996 <epigrf@outlook.com>
2023-08-31 21:36:04 +0800

    update asm rbp rsp

--- CppToolSet/CppWin32/Win32ApiTest/src/memory.hpp
+++ CppToolSet/CppWin32/Win32ApiTest/src/memory.hpp
@@ -11,28 +11,43 @@ namespace MemoryApiTest
 
     void Test()
     {
-        // stack info
-        NT_TIB* teb = reinterpret_cast<NT_TIB*>(NtCurrentTeb());
-        DWORD stackTop = reinterpret_cast<DWORD>(teb->StackBase);
-        DWORD stackBase = reinterpret_cast<DWORD>(teb->StackLimit);
-        DWORD distance = stackTop - stackBase;
-
-        std::cout << std::format("stack top: {:#x}", stackTop) << std::endl;
-        std::cout << std::format("stack base: {:#x}", stackBase) << std::endl;
-        std::cout << std::format("distance: {}", distance) << std::endl;
+        std::cout << "This stack frame" << std::endl;
 
         void* currentRbp;
         void* currentRsp;
 
         // Needs clang. MSVC x64 does not support inline assembly
-        _asm 
+        _asm
         {
             mov currentRbp, rbp
             mov currentRsp, rsp
         }
 
-        std::cout << std::format("rbp: {:#x}", reinterpret_cast<DWORD>(currentRbp)) << std::endl;
-        std::cout << std::format("rsp: {:#x}", reinterpret_cast<DWORD>(currentRsp)) << std::endl;
+        DWORD currentRbpValue = reinterpret_cast<DWORD>(currentRbp);
+        DWORD currentRspValue = reinterpret_cast<DWORD>(currentRsp);
+        DWORD distanceRbpRsp = currentRbpValue - currentRspValue;
+
+        std::cout << std::format("rbp: {:#x}", currentRbpValue) << std::endl;
+        std::cout << std::format("rsp: {:#x}", currentRspValue) << std::endl;
+        std::cout << std::format("distance: {} B", distanceRbpRsp) << std::endl
+
+        std::cout << std::endl;
+        std::cout << "This thread stack" << std::endl;
+
+        
+/*
+        
+        NT_TIB* teb = reinterpret_cast<NT_TIB*>(NtCurrentTeb());
+        DWORD allStackTop = reinterpret_cast<DWORD>(teb->StackBase);
+        DWORD stackBase = reinterpret_cast<DWORD>(teb->StackLimit);
+        DWORD distance = allStackTop - stackBase;
+
+        std::cout << std::format("stack top: {:#x}", allStackTop) << std::endl;
+        std::cout << std::format("stack base: {:#x}", stackBase) << std::endl;
+        std::cout << std::format("distance: {}", distance) << std::endl;
+    */    
+
+        
 
 
         
