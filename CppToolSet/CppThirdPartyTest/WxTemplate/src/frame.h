e209452c1c00b74bc66c9a32a8ab3c99498faa79
keqing996 <epigrf@outlook.com>
2023-09-11 14:14:54 +0800

    wxWidget template

---
+++ CppToolSet/CppThirdPartyTest/WxTemplate/src/frame.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <wx/wx.h>
+
+class AppFrame : public wxFrame
+{
+public:
+    AppFrame();
+
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
