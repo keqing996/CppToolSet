03e7e740432f85073ca6eeeffec6f5d76abc850b
keqing996 <epigrf@outlook.com>
2023-09-24 11:55:49 +0800

    adjust structure

---
+++ CppToolSet/CppGraphics/src/Renderer/RendererHardwareInterface/RenderHardwareInterface.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Define/RendererApi.h"
+#include "Util/NonCopyable.h"
+
+namespace Renderer
+{
+    class RenderHardwareInterface : public NonCopyable
+    {
+    public:
+        RenderHardwareInterface() = default;
+        virtual ~RenderHardwareInterface() = default;
+
+    public:
+        virtual bool SetUp() = 0;
+        virtual void Destroy() = 0;
+        virtual void Renderer() = 0;
+
+    public:
+        bool IsSettleUp() const;
+
+    protected:
+        bool _setUp = false;
+    };
+}
+
