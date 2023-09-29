11fdde52260d37ed52adff9a376ea5a87b27a272
keqing996 <epigrf@outlook.com>
2023-09-29 14:45:36 +0800

    update

--- CppToolSet/CppTools/MarkdownNoUseImgDeleter/src/main.cpp
+++ CppToolSet/CppTools/MarkdownNoUseImgDeleter/src/main.cpp
@@ -1,7 +1,25 @@
 
+#include <filesystem>
+#include <iostream>
+#include <regex>
+#include <unordered_set>
+#include <string>
 
 int main()
 {
+    std::unordered_set<std::string> _allImgSet;
+    std::unordered_set<std::string> _allMdFileSet;
+
+    auto currPath = std::filesystem::current_path();
+    for (const auto& f: std::filesystem::recursive_directory_iterator(currPath))
+    {
+        if (f.is_directory())
+            continue;
+
+        auto ext = f.path().extension();
+
+        std::cout << ext << std::endl;
+    }
 
     return 0;
 }
