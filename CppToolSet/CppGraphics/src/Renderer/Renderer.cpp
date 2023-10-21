945b39d1baee08d56f0e687bfa593ae3d9fcc2f2
keqing996 <epigrf@outlook.com>
2023-10-21 15:41:39 +0800

    new triangle

--- CppToolSet/CppGraphics/src/Renderer/Renderer.cpp
+++ CppToolSet/CppGraphics/src/Renderer/Renderer.cpp
@@ -33,26 +33,45 @@ namespace Renderer
 
     void Renderer::Render()
     {
-        constexpr LPCSTR pvsCode =
-                "#version 420 core\n"
-                "layout (location = 0) in vec3 aPos;\n"
-                "void main()\n"
-                "{\n"
-                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                "}\0";
+        constexpr LPCSTR pvsCode = R"(
+            #version 420 core
 
-        constexpr LPCSTR pfsCode =
-                "#version 420 core\n"
-                "out vec4 FragColor;\n"
-                "void main()\n"
-                "{\n"
-                "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                "}\n\0";
+            layout (location = 0) in vec3 a_Position;
+            layout (location = 1) in vec4 a_Color;
 
-        constexpr std::array<float, 9> Vert =  {
-                -0.5F, -0.5F, +0.0F, // left
-                +0.5F, -0.5F, +0.0F, // right
-                +0.0F, +0.5F, +0.0F  // top
+            out vec3 v_Position;
+            out vec4 v_Color;
+
+            void main()
+            {
+                v_Position = a_Position;
+                v_Color = a_Color;
+                gl_Position = vec4(a_Position, 1.0);
+            }
+)";
+
+        constexpr LPCSTR pfsCode = R"(
+            #version 420 core
+
+            layout (location = 0) out vec4 color;
+
+            in vec3 v_Position;
+            in vec4 v_Color;
+
+            void main()
+            {
+                color = vec4(v_Position * 0.5 + 0.5, 1.0);
+                color = v_Color;
+            }
+)";
+
+        constexpr std::array<float, 3 * (3 + 4)> Vert =  {
+                // left
+                -0.5f, -0.5f, 0.0f, 0.2f, 0.5f, 1.7f, 1.0f,
+                // right
+                0.5f, -0.5f, 0.0f, 0.1f, 0.6f, 0.3f, 1.0f,
+                // top
+                0.0f, 0.5f, 0.0f, 0.8f, 0.2f, 0.1f, 1.0f
         };
 
         constexpr std::array<unsigned int, 3> Indeices = { 0, 1, 2 };
@@ -88,7 +107,7 @@ namespace Renderer
         // Layout
         BufferLayout layout = {
                 BufferElement {ShaderDataType::Float3, "a_Position"},
-                //BufferElement {ShaderDataType::Float4, "a_Color"},
+                BufferElement {ShaderDataType::Float4, "a_Color"},
                 //BufferElement {ShaderDataType::Float3, "a_Normal"},
         };
 
