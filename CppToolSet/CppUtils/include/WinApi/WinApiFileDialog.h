b2cbcafec357552935f3e887216e4bf39ce8f106
keqing996 <epigrf@outlook.com>
2023-11-03 13:52:22 +0800

    open file

--- CppToolSet/CppUtils/include/WinApi/WinApiFileDialog.h
+++ CppToolSet/CppUtils/include/WinApi/WinApiFileDialog.h
@@ -1,8 +1,20 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace WinApi::FileDialog
 {
-    std::wstring OpenFile(const std::wstring& titleMsg);
+    struct FileTypeFilter
+    {
+        std::wstring name;
+        std::wstring suffix;
+    };
+
+    /*
+     * example { { L"Text Files", L"*.txt" }, { L"All Files", L"*.*" } }
+     */
+    std::wstring OpenFile(const std::wstring& titleMsg, const std::vector<FileTypeFilter>* pFilter = nullptr);
+
+    std::wstring OpenDirectory(const std::wstring& titleMsg);
 }
--- CppToolSet/CppUtils/src/WinApi/WinApiFileDialog.cpp
+++ CppToolSet/CppUtils/src/WinApi/WinApiFileDialog.cpp
@@ -3,10 +3,71 @@
 #include "../../include/WinApi/WinApiFileDialog.h"
 
 #include <shlobj.h>
+#include <shobjidl.h>
 
 namespace WinApi::FileDialog
 {
-    std::wstring OpenFile(const std::wstring& titleMsg)
+    std::wstring OpenFile(const std::wstring& titleMsg, const std::vector<FileTypeFilter>* pFilter)
+    {
+        std::wstring result;
+        bool getResultSuccess = false;
+        IFileDialog* pFileDialog;
+        HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+        if (SUCCEEDED(hr))
+        {
+            hr = CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL,
+                                  IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileDialog));
+            if (SUCCEEDED(hr))
+            {
+                // title
+                pFileDialog->SetTitle(titleMsg.c_str());
+
+                // filter
+                if (pFilter != nullptr)
+                {
+                    COMDLG_FILTERSPEC fileTypes[pFilter->size()];
+                    for (int i = 0; i < pFilter->size(); i++)
+                    {
+                        fileTypes[i].pszName = (*pFilter)[i].name.c_str();
+                        fileTypes[i].pszSpec = (*pFilter)[i].suffix.c_str();
+                    }
+                    pFileDialog->SetFileTypes(pFilter->size() * sizeof(COMDLG_FILTERSPEC), fileTypes);
+                }
+
+                // show
+                hr = pFileDialog->Show(nullptr);
+                if (SUCCEEDED(hr))
+                {
+                    IShellItem* pShellItem;
+                    hr = pFileDialog->GetResult(&pShellItem);
+                    if (SUCCEEDED(hr))
+                    {
+                        PWSTR filePath;
+                        hr = pShellItem->GetDisplayName(SIGDN_FILESYSPATH, &filePath);
+
+                        if (SUCCEEDED(hr))
+                        {
+                            result = filePath;
+                            getResultSuccess = true;
+                            CoTaskMemFree(filePath);
+                        }
+
+                        pShellItem->Release();
+                    }
+                }
+
+                pFileDialog->Release();
+            }
+            CoUninitialize();
+        }
+
+        if (getResultSuccess)
+            return result;
+
+        return {};
+    }
+
+    std::wstring OpenDirectory(const std::wstring& titleMsg)
     {
         TCHAR path[MAX_PATH];
         BROWSEINFO bi = { 0 };
