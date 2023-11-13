5691a15a7b6adcfc6bda10a3c5ed4a17af8b064b
keqing996 <epigrf@outlook.com>
2023-11-13 14:49:02 +0800

    thread safe logger

--- CppToolSet/CppUtils/include/DebugConsole.h
+++ CppToolSet/CppUtils/include/DebugConsole.h
@@ -2,45 +2,65 @@
 
 #include <string>
 #include <format>
+#include <fstream>
+#include <mutex>
 
 namespace Util
 {
-    class DebugConsole
+    class Logger
     {
     public:
-        DebugConsole() = delete;
+        Logger() = delete;
 
     public:
-        static void Init();
+        static void InitConsoleLogger();
+        static void InitFileLogger(const std::string& loggerPath);
+        static void Close();
 
+    public:
         static void LogInfo(const std::string& message);
         static void LogInfo(const std::string_view& message);
         static void LogInfo(const char* message);
 
-        template <class... _Types>
-        static void LogInfo(const std::format_string<_Types...> _Fmt, const _Types&... _Args)
+        template <class... Types>
+        static void LogInfo(const std::format_string<Types...> Fmt, const Types&... Args)
         {
-            LogInfo(std::format(_Fmt, _Args...));
+            LogInfo(std::format(Fmt, Args...));
         }
 
         static void LogWarn(const std::string& message);
         static void LogWarn(const std::string_view& message);
         static void LogWarn(const char* message);
 
-        template <class... _Types>
-        static void LogWarn(const std::format_string<_Types...> _Fmt, const _Types&... _Args)
+        template <class... Types>
+        static void LogWarn(const std::format_string<Types...> Fmt, const Types&... Args)
         {
-            LogWarn(std::format(_Fmt, _Args...));
+            LogWarn(std::format(Fmt, Args...));
         }
 
         static void LogError(const std::string& message);
         static void LogError(const std::string_view& message);
         static void LogError(const char* message);
 
-        template <class... _Types>
-        static void LogError(const std::format_string<_Types...> _Fmt, const _Types&... _Args)
+        template <class... Types>
+        static void LogError(const std::format_string<Types...> Fmt, const Types&... Args)
         {
-            LogError(std::format(_Fmt, _Args...));
+            LogError(std::format(Fmt, Args...));
         }
+
+    private:
+        static void WriteConsole(const std::string& message);
+        static void WriteConsole(const std::string_view& message);
+        static void WriteConsole(const char* message);
+
+        static void WriteFile(const std::string& message);
+        static void WriteFile(const std::string_view& message);
+        static void WriteFile(const char* message);
+
+    private:
+        inline static bool _enableConsoleLogger = false;
+        inline static bool _enableFileLogger = false;
+        inline static std::mutex _mutex = {};
+        inline static std::fstream* _pFileStream = nullptr;
     };
 }
--- CppToolSet/CppUtils/src/DebugConsole.cpp
+++ CppToolSet/CppUtils/src/DebugConsole.cpp
@@ -10,56 +10,118 @@
 
 namespace Util
 {
-    void DebugConsole::Init()
+    void Logger::InitConsoleLogger()
     {
 #ifdef BUILD_DEBUG
         WinApi::Console::CreateConsole();
         WinApi::Console::SetConsoleOutputUtf8();
+        _enableConsoleLogger = true;
 #endif
     }
 
-    void DebugConsole::LogInfo(const std::string& message)
+    void Logger::InitFileLogger(const std::string& loggerPath)
+    {
+        if (_pFileStream != nullptr)
+            return;
+
+        _pFileStream = new std::fstream(loggerPath, std::ios::out | std::ios::trunc);
+        if (!_pFileStream->is_open())
+        {
+            delete _pFileStream;
+            return;
+        }
+
+        _enableFileLogger = true;
+    }
+
+    void Logger::Close()
+    {
+        if (_pFileStream != nullptr)
+        {
+            if (_pFileStream->is_open())
+                _pFileStream->close();
+
+            delete _pFileStream;
+        }
+    }
+
+    void Logger::LogInfo(const std::string& message)
     {
         std::cout << message << std::endl;
     }
 
-    void DebugConsole::LogInfo(const std::string_view& message)
+    void Logger::LogInfo(const std::string_view& message)
     {
         std::cout << message << std::endl;
     }
 
-    void DebugConsole::LogInfo(const char* message)
+    void Logger::LogInfo(const char* message)
     {
         std::cout << message << std::endl;
     }
 
-    void DebugConsole::LogWarn(const std::string& message)
+    void Logger::LogWarn(const std::string& message)
     {
 
     }
 
-    static void LogWarn(const std::string_view& message)
+    void Logger::WriteConsole(const std::string& message)
     {
+#ifdef BUILD_DEBUG
+        if (!_enableConsoleLogger)
+            return;
 
+        std::lock_guard<std::mutex> guard(_mutex);
+        std::cout << message << std::endl;
+#endif
     }
 
-    static void LogWarn(const char* message)
+    void Logger::WriteConsole(const std::string_view& message)
     {
+#ifdef BUILD_DEBUG
+        if (!_enableConsoleLogger)
+            return;
 
+        std::lock_guard<std::mutex> guard(_mutex);
+        std::cout << message << std::endl;
+#endif
     }
 
-    static void LogError(const std::string& message)
+    void Logger::WriteConsole(const char* message)
     {
+#ifdef BUILD_DEBUG
+        if (!_enableConsoleLogger)
+            return;
 
+        std::lock_guard<std::mutex> guard(_mutex);
+        std::cout << message << std::endl;
+#endif
     }
 
-    static void LogError(const std::string_view& message)
+    void Logger::WriteFile(const std::string& message)
     {
+        if (!_enableFileLogger || _pFileStream == nullptr)
+            return;
 
+        std::lock_guard<std::mutex> guard(_mutex);
+        (*_pFileStream) << message << std::endl;
     }
 
-    static void LogError(const char* message)
+    void Logger::WriteFile(const std::string_view& message)
     {
+        if (!_enableFileLogger || _pFileStream == nullptr)
+            return;
 
+        std::lock_guard<std::mutex> guard(_mutex);
+        (*_pFileStream) << message << std::endl;
+    }
+
+    void Logger::WriteFile(const char* message)
+    {
+        if (!_enableFileLogger || _pFileStream == nullptr)
+            return;
+
+        std::lock_guard<std::mutex> guard(_mutex);
+        (*_pFileStream) << message << std::endl;
     }
 }
